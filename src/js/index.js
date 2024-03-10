3c815e64b7c3e8a157e0d4950aee58deb368d816
Halina Tulchii <halendeihalyna@gmail.com>
2024-03-10 07:30:20 +0200

    added part of swiper

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,28 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+  
+    // Navigation arrows
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    // },
+  
+   
+  });
+
+
 // import mobileNav from './modules/mobile-nav';
 // mobileNav();
 
